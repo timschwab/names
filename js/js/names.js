4604e073bbc4582d2499bd747baac1315390c9e2
Tim Schwab <tschwab@g.clemson.edu>
2017-06-08 15:35:17 -0400

    wrong NOT operator

--- js/js/names.js
+++ js/js/names.js
@@ -1,7 +1,7 @@
 var temp = true;
 
 function generateInsult() {
-	temp = ~temp;
+	temp = !temp;
 	if (temp)
 		return "Away, you starvelling, you elf-skin, you dried neat's-tongue, bull's-pizzle, you stock-fish!";
 	else
