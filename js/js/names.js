aa27bfa667bcfe6e7751f6c326f0c56fe1556cff
Tim Schwab <tschwab@g.clemson.edu>
2017-06-08 19:45:23 -0400

    Enhanced grammar

--- js/js/names.js
+++ js/js/names.js
@@ -1,18 +1,29 @@
-var temp = true;
 
-/* Entry point */
+
+/*
+	Entry point
+*/
+
 function generateInsult() {
-	var terminals = runProductions([new Insult()]);
+	var terminals = runProductions([new Insult]);
 	var insult = resolveTerminals(terminals);
 
 	return insult;
 }
 
+
+
+/*
+	Logic
+*/
+
 function runProductions(list) {
+	console.log(list);
 	var newList = new Array();
 	var allTerminals = true;
 
 	list.forEach((symbol) => {
+		console.log(symbol);
 		if (symbol.terminal) {
 			newList.push(symbol);
 		} else {
@@ -30,50 +41,65 @@ function runProductions(list) {
 function resolveTerminals(list) {
 	var result = "";
 	list.forEach((terminal) => {
-		result += terminal.resolve();
+		result += terminal.value;
 	});
 
 	return result;
 }
 
+function selectProduction(productions) {
+	var n = Math.floor(Math.random() * productions.length);
+	var constructorList = productions[n];
+	var objList = [];
+	constructorList.forEach((constructor) => {
+		objList.push(new constructor);
+	});
+	return objList;
+}
+
 
 
 /*
-	PRODUCTIONS
+	Nonterminals
 */
 
-function Insult() {
-	this.terminal = false;
-	this.resolve = () => {
-		var n = Math.random();
-		if (n < 0.333)
-			return [new A()];
-		else if (n < 0.667)
-			return [new B()];
-		else
-			return [new Insult(), new Space(), new Insult()];
-	};
+class Insult {
+	constructor() {
+		this.terminal = false;
+		this.resolve = () => selectProduction(this.productions);
+		this.productions =
+		[
+			[A],
+			[B],
+			[Insult, Space, Insult]
+		];
+	}
 }
 
-function Space() {
-	this.terminal = true;
-	this.resolve = () => {
-		return " ";
-	};
+
+
+/*
+	Terminals
+*/
+
+class Space {
+	constructor() {
+		this.terminal = true;
+		this.value = " ";
+	}
 }
 
-function A() {
-	this.terminal = true;
-	this.resolve = () => {
-		return "AAAAA";
-	};
+class A {
+	constructor() {
+		this.terminal = true;
+		this.value = "A";
+	}
 }
 
-function B() {
-	this.terminal = true;
-	this.resolve = () => {
-		return "BBBBB";
-	};
+class B {
+	constructor() {
+		this.terminal = true;
+		this.value = "B";
+	}
 }
-
 
