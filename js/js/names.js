614d850b2936ebcf84641730c3a48ba6b1531186
Tim Schwab <tschwab@g.clemson.edu>
2017-06-09 10:14:00 -0400

    Translate existing grammar into new form

--- js/js/names.js
+++ js/js/names.js
@@ -47,115 +47,35 @@ function Token(key, values) {
 
 tokens.push(new Token(
 	"<Insult>",
-	["<Insult> <Insult>", "A", "B"]));
+	["<SubjectPhrase> <VerbPhrase>."]));
 
+tokens.push(new Token(
+	"<SubjectPhrase>",
+	["<Name>", "The <AdjectivePhrase> <Name>", "<Name>, <PrepositionalPhrase>, "]));
 
+tokens.push(new Token(
+	"<AdjectivePhrase>",
+	["<Adjective>", "<Adverb> <Adjective>"]));
 
+tokens.push(new Token(
+	"<PrepositionalPhrase>",
+	["<Preposition> the <NounPhrase>"]));
 
+tokens.push(new Token(
+	"<NounPhrase>",
+	["<Noun>", "<AdjectivePhrase> <Noun>"]));
 
+tokens.push(new Token(
+	"<VerbPhrase>",
+	["<SimpleVerbPhrase>", "<DirectObjectVerbPhrase>"]));
 
+tokens.push(new Token(
+	"<SimpleVerbPhrase>",
+	["<Verb>", "<Adverb> <Verb>"]));
 
-class Nonterminal {
-	constructor() {
-		this.terminal = false;
-		this.resolve = () => {
-			var objList = [];
-			var constructorList = randomElement(this.productions);
-			constructorList.forEach((constructor) => {
-				objList.push(new constructor);
-			});
-			return objList;
-		};
-	}
-}
-
-class Insult extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[SubjectPhrase, Space, VerbPhrase, Punctuation]
-		];
-	}
-}
-
-class SubjectPhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[Name],
-			[CapitalThe, Space, AdjectivePhrase, Space, Name],
-			[Name, Comma, Space, PrepositionalPhrase, Comma, Space]
-		];
-	}
-}
-
-class AdjectivePhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[Adjective],
-			[Adverb, Space, Adjective]
-		];
-	}
-}
-
-class PrepositionalPhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[Preposition, Space, The, Space, NounPhrase]
-		];
-	}
-}
-
-class NounPhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[Noun],
-			[AdjectivePhrase, Space, Noun]
-		];
-	}
-}
-
-class VerbPhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[SimpleVerbPhrase],
-			[DirectObjectVerbPhrase]
-		];
-	}
-}
-
-class SimpleVerbPhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[Verb],
-			[Adverb, Space, Verb],
-			[Verb, Space, Adverb]
-		];
-	}
-}
-
-class DirectObjectVerbPhrase extends Nonterminal {
-	constructor() {
-		super();
-		this.productions =
-		[
-			[DirectObjectVerb, Space, The, Space, NounPhrase],
-			[Adverb, Space, DirectObjectVerb, Space, The, Space, NounPhrase]
-		];
-	}
-}
+tokens.push(new Token(
+	"<DirectObjectVerbPhrase>",
+	["<DirectObjectVerb> the <NounPhrase>", "<Adverb> <DirectObjectVerb> the <NounPhrase>"]));
 
 
 
@@ -163,139 +83,32 @@ class DirectObjectVerbPhrase extends Nonterminal {
 	Terminals
 */
 
-class Terminal {
-	constructor() {
-		this.terminal = true;
-		this.value = () => randomElement(this.values);
-	}
-}
+tokens.push(new Token(
+	"<Name>",
+	["Tim", "Ben", "Andrew", "Will", "Dorothy"]));
 
-class Space extends Terminal {
-	constructor() {
-		super();
-		this.values = [" "];
-	}
-}
+tokens.push(new Token(
+	"<Noun>",
+	["octopus", "distraction", "philosopher", "advantage", "wallpaper"]));
 
-class Punctuation extends Terminal {
-	constructor() {
-		super();
-		this.values = ["."];
-	}
-}
+tokens.push(new Token(
+	"<Verb>",
+	["laughs", "complains", "tumbles", "explodes", "leaves the area"]));
 
-class Comma extends Terminal {
-	constructor() {
-		super();
-		this.values = [","];
-	}
-}
+tokens.push(new Token(
+	"<DirectObjectVerb>",
+	["avoids", "does not like", "laughs at", "is perplexed by"]));
 
-class The extends Terminal {
-	constructor() {
-		super();
-		this.values = ["the"];
-	}
-}
+tokens.push(new Token(
+	"<Adjective>",
+	["indescribable", "upside-down", "uncomfortable", "silly", "nauseating", "miniscule"]));
 
-class CapitalThe extends Terminal {
-	constructor() {
-		super();
-		this.values = ["The"];
-	}
-}
+tokens.push(new Token(
+	"<Adverb>",
+	["tremendously", "awkwardly", "occasionally", "preposterously", "dejectedly"]));
 
-class Name extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"Tim",
-			"Ben",
-			"Andrew",
-			"Will",
-			"Dorothy"
-		];
-	}
-}
-
-class Noun extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"octopus",
-			"distraction",
-			"philosopher",
-			"advantage",
-			"wallpaper"
-		];
-	}
-}
-
-class Verb extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"laughs",
-			"whines",
-			"tumbles",
-			"explodes",
-			"leaves the area"
-		];
-	}
-}
-
-class DirectObjectVerb extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"avoids",
-			"does not like",
-			"laughs at",
-			"is perplexed by",
-			"is"
-		];
-	}
-}
-
-class Adjective extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"indescribable",
-			"upside-down",
-			"uncomfortable",
-			"silly",
-			"nauseating",
-			"miniscule"
-		];
-	}
-}
-
-class Adverb extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"tremendously",
-			"awkwardly",
-			"occasionally",
-			"preposterously",
-			"quite",
-			"dejectedly"
-		];
-	}
-}
-
-class Preposition extends Terminal {
-	constructor() {
-		super();
-		this.values = [
-			"with",
-			"amongst",
-			"by means of",
-			"despite",
-			"underneath"
-		];
-	}
-}
+tokens.push(new Token(
+	"<Preposition>",
+	["with", "amongst", "by means of", "despite", "underneath"]));
 
 
